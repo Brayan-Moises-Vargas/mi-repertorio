f6eeb4764057023e87ed827bb1137cdf2c900d00
Brayan vargas <bvargas.segredo@gmail.com>
2023-12-17 21:39:17 -0300

    add update

--- index.js
+++ index.js
@@ -49,6 +49,33 @@ app.post("/canciones", (req, res) => {
   }
 });
 
+app.put("/canciones/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const datosActualizados = req.body;
+
+    const canciones = JSON.parse(fs.readFileSync("repertorio.json", "utf8"));
+
+    const cancionIndex = canciones.findIndex((c) => c.id === parseInt(id));
+    if (cancionIndex !== -1) {
+      canciones[cancionIndex] = {
+        id: parseInt(id),
+        titulo: datosActualizados.titulo,
+        artista: datosActualizados.artista,
+        tono: datosActualizados.tono,
+      };
+
+      fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
+
+      res.status(200).json({ message: "Canción actualizada" });
+    } else {
+      res.status(404).json({ message: "Canción no encontrada" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: "Error interno del servidor" });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo 🚀  http://localhost:${PORT}`);
 });
